c9110e03e1ec5ba670b72eb9033e588026b77513
Greg Mahone <gmahone@gmail.com>
2022-10-30 21:35:42 +0100

    add get_rank function

--- functions.R
+++ functions.R
@@ -1,1 +1,12 @@
+get_rank <- function(x, decreasing=FALSE){
+  if(!is.numeric(x)){
+    return(cat("Input not numeric!"))
+  }
+  blank_vec <- rep(NA, length(x))
+  tmp_order <- order(x, decreasing=decreasing)
+  tmp_rank <- blank_vec
+  tmp_rank[tmp_order] <- 1:length(x)
+  return(tmp_rank)
+}
+
 
