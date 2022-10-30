78e90a9e4dcf146e1613bb2f1e27a2aac4b9ff53
Greg Mahone <gmahone@gmail.com>
2022-10-30 21:36:59 +0100

    add create_lsd_table function

--- functions.R
+++ functions.R
@@ -22,3 +22,32 @@ rename.column <- function(x, y, data){
   colnames(data)[which(colnames(data) %in% x)] <- y
   return(data)
 }
+
+
+### arguments
+# means_df - dataframe with means, 1st column id, 2nd column means, other columns ignored
+# lsd - lsd value
+# decreasing - trait directionality. should the values decrease (default) or increase?
+create_lsd_table <- function(means_df, lsd, decreasing=TRUE){
+  means_df <- means_df[order(means_df[,2], decreasing=decreasing),]
+  diff_mat <- outer(means_df[,2], means_df[,2], "-")
+  init_mat <- matrix(NA, nrow=nrow(diff_mat), ncol=ncol(diff_mat))
+  cur_letter <- 1
+  for(i in 1:nrow(diff_mat)){
+    not_sig_diff <- diff_mat[i,] < lsd
+    if(i == 1){
+      last_length <- sum(not_sig_diff)
+      init_mat[i, not_sig_diff] <- LETTERS[cur_letter]
+    } else {
+      cur_length <- sum(not_sig_diff)
+      if(cur_length != last_length){
+        cur_letter <- cur_letter + 1
+        init_mat[i, not_sig_diff] <- LETTERS[cur_letter]
+        last_length <- cur_length
+      }
+    }
+  }
+  init_mat[lower.tri(init_mat)] <- NA
+  means_df$Groups <- apply(init_mat, 2, function(x) paste(as.vector(na.omit(x)), sep="", collapse=","))
+  return(means_df)
+}
